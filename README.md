715ce25139f0ddf8d63d3f9e6cb31eaadf84c08c
Li Yiu Shing <18022213d@connect.polyu.hk>
2020-11-18 16:42:42 +0800

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# COMP4142 Group Project
+
+## Introduction
+
+It built with Express("~4.16.1").
+
+## Pre-require
+NODE 
+https://nodejs.org/en/
+GIT
+https://git-scm.com/book/en/v2/Getting-Started-Installing-Git
+
+
+## Getting Started
+
+```
+cd comp4142
+npm install
+npm start
+```
+
+And Browse http://localhost:4000/
+
+## Git Commit the code
+```
+git 
+
+```
---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var cors = require("cors");
+var bodyParser = require("body-parser");
+
+var indexRouter = require("./routes/index");
+
+// var connect = require("./database");
+
+var app = express();
+
+// Middleware
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Router
+app.use("/", indexRouter);
+
+app.listen(4000, () => console.log("Server Up and running at 4000"));
+
+module.exports = app;
